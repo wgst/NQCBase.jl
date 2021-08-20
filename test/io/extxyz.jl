b4d16bab3f588608d915856922dbfd4c10811a95
James Gardner <james.gardner1421@gmail.com>
2021-08-20 17:29:01 +0100

    Fix io

--- test/io/extxyz.jl
+++ test/io/extxyz.jl
@@ -2,10 +2,20 @@
 using Test
 using NonadiabaticDynamicsBase
 
+atoms = Atoms([:H, :C, :O, :N])
+cell = PeriodicCell(rand(3, 3) .* 10)
+display(au_to_ang.(cell.vectors))
+R = rand(3, 4) .* 10
+
+@testset "to/from_extxyz_dict" begin
+    dict = NonadiabaticDynamicsBase.to_extxyz_dict(atoms, R, cell)
+    @test dict["cell"] ≈ au_to_ang.(permutedims(cell.vectors, (2,1)))
+    natoms, nR, ncell = NonadiabaticDynamicsBase.from_extxyz_dict(dict)
+    @test ncell.vectors ≈ cell.vectors
+    @test ncell.inverse ≈ cell.inverse
+end
+
 @testset "Single frame" begin
-    atoms = Atoms([:H, :C, :O, :N])
-    cell = PeriodicCell([10 0 0; 0 10 0; 0 0 10])
-    R = rand(3, 4) .* 10
     write_extxyz("output.xyz", atoms, R, cell)
     new_atoms, new_R, new_cell = read_extxyz("output.xyz")
     @test new_atoms == atoms
@@ -17,7 +27,7 @@ end
 
 @testset "Multiple frames" begin
     atoms = Atoms([:H, :C, :O, :N])
-    cell = PeriodicCell([10 0 0; 0 10 0; 0 0 10])
+    cell = PeriodicCell(rand(3, 3) .* 10)
     R = [rand(3, 4) .* 10 for _=1:100]
     write_extxyz("output.xyz", atoms, R, cell)
     new_atoms, new_R, new_cell = read_extxyz("output.xyz")
