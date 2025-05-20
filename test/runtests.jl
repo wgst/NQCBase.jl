6ecbdb85a58fda76bf4dc80565c0bb84dae2d627
Alexander Spears <39826690+Alexsp32@users.noreply.github.com>
2025-05-20 11:18:21 +0100

    ➖ Evict python-dependent features to separate package (#24)
    * ✅ Move ase tests to NQCDInterfASE
    
    * ⬆️ Fix dependency issues?
    
    * 🔖 Version bump v0.3.1 for PR
    
    * ⚰️ Remove evicted Python interfaces
    
    * ⬆️ Upgrade AtomsBase v0.5
    
    * Revert "⬆️ Upgrade AtomsBase v0.5" due to compat issues
    
    This reverts commit d846d26c2de1db43475230ee4eb7635ab1edd1fe.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,5 +4,4 @@ using Test, SafeTestsets
 @safetestset "Atoms tests" begin include("atoms.jl") end
 @safetestset "Cells tests" begin include("cells.jl") end
 @safetestset "ExtXYZ tests" begin include("io/extxyz.jl") end
-@safetestset "ase tests" begin include("io/ase.jl") end
 @safetestset "AtomsBase tests" begin include("atoms_base.jl") end
